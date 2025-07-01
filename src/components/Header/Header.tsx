4017f48ad60b08f90b9a37d2c396291581c10994
marina-sheigets <sejmar1705@gmail.com>
2025-07-01 22:37:34 +0300

    feat: add simple layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavigationMenu from "../NavigationMenu/NavigationMenu";
+
+function Header() {
+  return (
+    <div>
+      <NavigationMenu />
+    </div>
+  );
+}
+
+export default Header;
