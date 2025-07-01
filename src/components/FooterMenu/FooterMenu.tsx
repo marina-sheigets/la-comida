829dca056414111f030f4752a4d339b380dc1502
marina-sheigets <sejmar1705@gmail.com>
2025-07-01 23:14:41 +0300

    feat: add footer menu

--- src/components/FooterMenu/FooterMenu.tsx
+++ src/components/FooterMenu/FooterMenu.tsx
@@ -1,5 +1,43 @@
+"use client";
+import { useRouter } from "next/navigation";
+import styles from "./FooterMenu.module.css";
+
 function FooterMenu() {
-  return <div>FooterMenu</div>;
+  const router = useRouter();
+
+  const handleNavigateToAllRecipes = () => {
+    router.push("/recipes");
+  };
+
+  const handleNavigateToMyCookbook = () => {
+    router.push("/my-cookbook");
+  };
+
+  const handleNavigateToExperts = () => {
+    router.push("/experts");
+  };
+
+  return (
+    <div>
+      <ul className={styles.footerMenu}>
+        <li>
+          <button type="button" onClick={handleNavigateToAllRecipes}>
+            Всі рецепти
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={handleNavigateToMyCookbook}>
+            Moя кулінарна книга
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={handleNavigateToExperts}>
+            Експерти
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default FooterMenu;
