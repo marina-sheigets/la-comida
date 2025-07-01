bfbd2d6aa64cdf2c35994f8ee171941bd8746970
marina-sheigets <sejmar1705@gmail.com>
2025-07-01 22:59:22 +0300

    feat: add menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,5 +1,50 @@
+"use client";
+import { useRouter } from "next/navigation";
+import styles from "./Menu.module.css";
+
 function Menu() {
-  return <div>Menu</div>;
+  const router = useRouter();
+
+  const handleNavigateToRecipes = () => {
+    router.push("/recipes");
+  };
+  const handleNavigateToCreateRecipe = () => {
+    router.push("/create-recipe");
+  };
+  const handleNavigateToMyCookbook = () => {
+    router.push("/my-cookbook");
+  };
+  return (
+    <ul className={styles.menu}>
+      <li>
+        <button
+          type="button"
+          className={styles.menuButton}
+          onClick={handleNavigateToRecipes}
+        >
+          Всі рецепти
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          className={styles.menuButton}
+          onClick={handleNavigateToCreateRecipe}
+        >
+          Створити рецепт
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          className={styles.menuButton}
+          onClick={handleNavigateToMyCookbook}
+        >
+          Моя кулінарна книга
+        </button>
+      </li>
+    </ul>
+  );
 }
 
 export default Menu;
